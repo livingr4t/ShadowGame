41a3d58c81b2c205117c8494d8553de8e75f8a8b
livingr4t <32771640+livingr4t@users.noreply.github.com>
2025-03-28 23:31:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ import os
 
 app = Flask(__name__)
 
+# Główna strona startowa gry
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Trasa dla uruchomienia gry
+@app.route('/game')
+def game():
+    from game_logic import main_game_loop
+    main_game_loop()
+    return "Gra została zakończona. Odśwież stronę, aby zagrać ponownie."
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
